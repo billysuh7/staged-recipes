3caddb134af2643de89fba3087b5f1a42cf9af3b
Billy Suh <bsuh@nvidia.com>
2024-12-12 00:33:37 +0000

    generalize it for different arch types

--- recipes/holoscan/compile_hello_world.sh
+++ recipes/holoscan/compile_hello_world.sh
@@ -2,10 +2,13 @@
 
 set -ex
 
+[[ ${target_platform} == "linux-64" ]] && targetsDir="targets/x86_64-linux"
+[[ ${target_platform} == "linux-aarch64" ]] && targetsDir="targets/sbsa-linux"
+
 cp -vf $PREFIX/examples/hello_world/cpp/CMakeLists.txt .
 cp -vf $PREFIX/examples/hello_world/cpp/hello_world.cpp .
 
-# $CONDA_PREFIX/libexec/gcc/x86_64-conda-linux-gnu/13.3.0/cc1plus
+# E.g. $CONDA_PREFIX/libexec/gcc/x86_64-conda-linux-gnu/13.3.0/cc1plus
 find $CONDA_PREFIX -name cc1plus
 
 GCC_DIR=$(dirname $(find $CONDA_PREFIX -name cc1plus))
@@ -13,16 +16,17 @@ GCC_DIR=$(dirname $(find $CONDA_PREFIX -name cc1plus))
 export PATH=${GCC_DIR}:$PATH
 export LD_LIBRARY_PATH=${GCC_DIR}:$LD_LIBRARY_PATH
 
+# No need for use-linker-plugin optimization, causes compile failure, don't use it for the test
 export CXXFLAGS="${CXXFLAGS} -fno-use-linker-plugin"
 
-$PREFIX/bin/x86_64-conda-linux-gnu-c++ --version
+echo CC =  $CC
+echo CXX =  $CXX
 
 cmake . \
   -DCMAKE_LIBRARY_PATH=${GCC_DIR} \
-  -DCMAKE_C_COMPILER=$PREFIX/bin/x86_64-conda-linux-gnu-gcc \
-  -DCMAKE_CXX_COMPILER=$PREFIX/bin/x86_64-conda-linux-gnu-g++ \
-  -DCUDAToolkit_INCLUDE_DIRECTORIES="$PREFIX/include;$PREFIX/targets/x86_64-linux/include" \
-  -DCUDAToolkit_LIBRARY_DIRECTORIES="$PREFIX/lib;${GCC_DIR}"
+  -DCMAKE_C_COMPILER=$CC \
+  -DCMAKE_CXX_COMPILER=$CXX \
+  -DCUDAToolkit_INCLUDE_DIRECTORIES="$PREFIX/include;$PREFIX/${targetsDir}/include"
 
 cmake --build .
 
